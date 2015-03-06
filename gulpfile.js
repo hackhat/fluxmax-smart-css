68ff3629ee54f60d0f6666ec3f3e246a35870bbd
hackhat <hackhat.com@gmail.com>
2015-03-06 14:51:55 +0000

    Adds build process and outputs a build app.

--- gulpfile.js
+++ gulpfile.js
@@ -15,40 +15,42 @@ var absPath = function(pathString){
 
 
 
+var config = {
+    entry: {
+        client : "./src/index.js"
+    },
+    output: {
+        path          : absPath("/dist"),
+        filename      : "[chunkhash].js",
+        chunkFilename : "[id].[chunkhash].js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/,
+                loader: "url-loader?limit=10000"
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            // fluxmax         : absPath('../../src/index'),
+            'event-emitter' : 'wolfy87-eventemitter',
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template : './index.html',
+        })
+    ],
+    devtool: 'eval',
+    debug: true,
+}
+
+
+
 gulp.task("default", ["webpack-dev-server"]);
 gulp.task("webpack-dev-server", function(callback){
-    // modify some webpack config options
-    var config = {
-        entry: {
-            client : "./src/index.js"
-        },
-        output: {
-            path          : absPath("/dist"),
-            filename      : "[chunkhash].js",
-            chunkFilename : "[id].[chunkhash].js"
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/,
-                    loader: "url-loader?limit=10000"
-                },
-            ]
-        },
-        resolve: {
-            alias: {
-                // fluxmax         : absPath('../../src/index'),
-                'event-emitter' : 'wolfy87-eventemitter',
-            },
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template : './index.html',
-            })
-        ]
-    }
-    config.devtool = "eval";
-    config.debug = true;
 
     // Start a webpack-dev-server
     new WebpackDevServer(webpack(config), {
@@ -60,5 +62,14 @@ gulp.task("webpack-dev-server", function(callback){
     }).listen(8080, "localhost", function(err) {
         if(err) throw new gutil.PluginError("webpack-dev-server", err);
         gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
+});
+
+gulp.task("build", function(callback){
+    webpack(config, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack:build", err);
+        gutil.log("[webpack:build]", stats.toString({
+            colors: true
+        }));
     });
 });
